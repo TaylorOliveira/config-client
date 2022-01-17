ec12139ef213ccb289103e6d402e561e7a55cb8a
Taylor <uaitayloroliveira@gmail.com>
2022-01-17 19:48:51 -0300

    Initial structure config client.

---
+++ src/test/java/br/com/springcloudconfigclient/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springcloudconfigclient.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
